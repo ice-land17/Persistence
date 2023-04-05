f34e311c46abd835f1bfa63c0cd85d0e721446b9
ice-land17 <iceland017@qq.com>
2023-04-05 10:18:03 +0800

    代码测试

---
+++ Project2.c
@@ -0,0 +1,122 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//extern int g_val;
+//int main()
+//{
+//	printf("%d\n", g_val);
+//	return 0;
+//}
+//#include<stdio.h>
+//enum Sex
+//{
+//	MALE=3,
+//	FEMALE,
+//	SECRET
+//};
+//
+//int main()
+//{
+//	enum Sex s = MALE;
+//	printf("%d\n", MALE);
+//	printf("%d\n", FEMALE);
+//	printf("%d\n", SECRET);
+//
+//	return 0;
+//}
+//#include <stdio.h>
+
+//int main()
+//{	
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c', };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+//#include<stdio.h>
+//#include <string.h>
+//int main() {
+//	printf("%d\n",strlen("c:\test\328\test.c"));
+//	return 0;
+//}
+//#include<stdio.h>
+//int main() 
+//{
+//	int input = 0;
+//	printf("郭玲雨是SB？:>\n");
+//	printf("那她是sb吗(1/0)?>:");
+//	scanf_s("%d", &input);
+//	if (input == 1)
+//		printf("她是SB\n");
+//	else
+//		printf("她真的是SB\n");
+//	return 0;
+//}
+
+//int main() {
+//	int line = 0;
+//	while (line<30000)
+//	{
+//		printf("写代码:%d\n", line);
+//		line++;
+//	}
+//	if (line==30000)
+//	{
+//		printf("好offer\n");
+//	}
+//	return 0;
+//}
+
+//int main() {
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int sum = num1 + num2;
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//Add(int x, int y)	
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main() {
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int sum = Add(num1, num2);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	return (x + y);
+//}
+//int main() {
+//	int n1 = 0;
+//	int n2 = 0;
+//	//输入
+//	scanf("%d%d", &n1, &n2);
+//	//求和
+//	int sum = Add(n1, n2);
+//	int t1 = 100;
+//	int t2 = 300;
+//	int ret = Add(t1, t2);
+//	//输入加值
+//	printf("%d\n", sum);
+//	return 0;
+//}
+//int main() {
+//	int arr[15] = { 11,12,13,14,15,16,17,18,19,20,21,22,23,24 };
+//	int i = 0;
+//	while (i < 15)
+//	{
+//		printf("%d ", arr[i]);
+//		i = i + 1;
+//	}
+//	return 0;
+//}

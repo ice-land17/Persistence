87ab3ec9ba750f55ff801e996564c0f0827486f2
iceland_17 <128897003+ice-land17@users.noreply.github.com>
2023-04-09 18:14:57 +0800

    Add files via upload

---
+++ Test1-5分支和循环语句(1)/Loop statement.c
@@ -0,0 +1,117 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//While循环
+
+//int main()
+//{
+//	while (1)
+//		printf("hehe\n");
+//	return 0;
+//}
+
+
+//在屏幕上打印1到10的数字
+//总结:break在while循环中的作用:
+//其实在循环中只要遇到break, 就停止后期的所有的循环，直接终止循环。
+//所以 : while中的break是用于永终止循环的。
+
+//int main()
+//{
+//	int i = 1;
+//	while (i<=10)
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+//getchar函数用法:可以适当的清理缓冲区
+//int main()
+//{
+//	char password[20] = { 0 };
+//	printf("请输入密码:>");
+//	scanf("%s", password);
+//
+//	int tmp = 0;
+//	while ((tmp = getchar()) != '\n')
+//	{
+//		;
+//	}
+//	printf("请确认密码(Y/N):>");
+//		int ch = getchar();
+//	if ('Y' == ch)
+//		printf("确认成功\n");
+//	else
+//		printf("确认成功\n");
+//}
+
+
+
+////for 循环
+////打印1到10
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		printf("%d ",i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (6 == i)
+//		break;
+//		printf("%d ", i);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int k = 0;
+//	for (i = 0, k = 0; k = 0; i++, k++)
+//		k++;
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//for
+
+
+
+
+
+
+
+//do while
+

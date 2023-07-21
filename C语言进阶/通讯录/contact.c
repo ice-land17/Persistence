2de19831a58462796cb03d3cf9eafc37c8474add
iceland_17 <128897003+ice-land17@users.noreply.github.com>
2023-07-21 12:21:08 +0800

    Add files via upload

---
+++ C语言进阶/通讯录/contact.c
@@ -0,0 +1,106 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "contact.h"
+
+//静态的版本
+void InitContact(Contact* pc)
+{
+	assert(pc);
+	pc->sz = 0;
+	memset(pc->data, 0, sizeof(pc->data));
+}
+
+
+void AddContact(Contact* pc)
+{
+	assert(pc);
+	if (pc->sz == MAX)
+	{
+		printf("通讯录已满，无法添加\n");
+		return;
+	}
+	//录入信息
+	printf("请输入名字:>");
+	scanf("%s", pc->data[pc->sz].name);
+	printf("请输入年龄:>");
+	scanf("%d", &pc->data[pc->sz].age);
+	printf("请输入性别:>");
+	scanf("%s", pc->data[pc->sz].sex);
+	printf("请输入电话:>");
+	scanf("%s", pc->data[pc->sz].tele);
+	printf("请输入地址:>");
+	scanf("%s", pc->data[pc->sz].addr);
+	pc->sz++;
+	printf("添加成功\n");
+}
+void PrintContact(const Contact* pc)
+{
+	assert(pc);
+
+	int i = 0;
+	printf("%-20s %-5s %-5s %-12s %-30s\n", "姓名", "年龄", "性别", "电话", "地址");
+
+	for (i = 0; i < pc->sz; i++)
+	{
+		printf("%-20s %-5d %-5s %-12s %-30s\n", pc->data[i].name, pc->data[i].age, pc->data[i].sex, pc->data[i].tele, pc->data[i].addr);
+	}
+}
+int FindByName(const Contact* pc, char name[])
+{
+	assert(pc);
+	int i = 0;
+	for (i = 0; i < pc->sz; i++)
+	{
+		if (0 == strcmp(pc->data[i].name, name))
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+void DelContact(Contact* pc)
+{
+	assert(pc);
+
+	if (pc->sz == 0)
+	{
+		printf("通讯录已空，无法删除\n");
+		return;
+	}
+	//删除
+	//1. 找到
+	char name[NAME_MAX] = { 0 };
+	printf("请输入要删除人的名字:>");
+	scanf("%s", name);
+	int pos = FindByName(pc, name);
+	if (pos == -1)
+	{
+		printf("要删除的人不存在\n");
+		return;
+	} 
+	//2. 删除
+	int j = 0;
+	for (j = pos; j < pc->sz - 1; j++)
+	{
+		pc->data[j] = pc->data[j + 1];
+	}
+
+	pc->sz--;
+	printf("删除成功\n");
+}
+
+void SearchContact(const Contact* pc)
+{
+	char name[NAME_MAX] = { 0 };
+	printf("请输入要查找人的名字:>");
+	scanf("%s", name);
+	int pos = FindByName(pc, name);
+	if (pos == -1)
+	{
+		printf("要查找的人不存在\n");
+		return;
+	}
+	printf("%-20s %-5s %-5s %-12s %-30s\n", "姓名", "年龄", "性别", "电话", "地址");
+	printf("%-20s %-5d %-5s %-12s %-30s\n", pc->data[pos].name, pc->data[pos].age, pc->data[pos].sex,
+		pc->data[pos].tele, pc->data[pos].addr);
+}
---
+++ C语言进阶/通讯录/test.c
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "contact.h"
+
+void menu()
+{
+	printf("***************************\n");
+	printf("**** 1.add    2.del    ****\n");
+	printf("**** 3.search 4.modify ****\n");
+	printf("**** 5.sort   6.print  ****\n");
+	printf("**** 0.exit            ****\n");
+	printf("***************************\n");
+}
+void test()
+{
+	int input = 0;
+	//创建通讯录
+	Contact con;
+	//初始化通讯录
+	InitContact(&con);
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;  
+		case MODIFY:
+			break;
+		case SORT:
+			break;
+		case PRINT:
+			PrintContact(&con);
+			break;
+		case EXIT:
+			printf("退出通讯录");
+			break;
+		default:
+			printf("选择错误");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}

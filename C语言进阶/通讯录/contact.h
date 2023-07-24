71e4ccc65224f44cb9f1deee659720063475f331
iceland_17 <128897003+ice-land17@users.noreply.github.com>
2023-07-24 18:55:37 +0800

    Add files via upload

---
+++ C语言进阶/通讯录/contact.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <string.h>
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+//类型的声明
+
+#define MAX 1000
+#define NAME_MAX 20
+#define SEX_MAX 5
+#define TELE_MAX 12
+#define ADDR_MAX 30
+
+//通讯录初始状态的容量大小
+#define DEAFUALT_SZ 3
+
+enum Option
+{
+	EXIT,//0
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SORT,
+	PRINT
+};
+typedef struct PeoInfo
+{
+	char name[NAME_MAX];
+	char sex[SEX_MAX];
+	int age;
+	char tele[TELE_MAX];
+	char addr[ADDR_MAX];
+}PeoInfo;
+
+////静态的版本
+//typedef struct Contact
+//{
+//	PeoInfo data[MAX];//可以存放1000个人的信息
+//	int sz;//记录通讯中已经保存的信息个数
+//}Contact;
+
+//动态的版本
+typedef struct Contact
+{
+	PeoInfo* data;//可以存放1000个人的信息
+	int sz;//记录通讯中已经保存的信息个数
+	int capacity;//记录通讯录当前的最大容量
+}Contact;
+
+//函数的声明
+
+//初始化通讯录
+void InitContact(Contact* pc);
+
+//销毁通讯录
+void DestroyContact(Contact* pc);
+
+//增加联系人信息
+void AddContact(Contact* pc);
+
+//打印联系人信息
+void PrintContact(const Contact* pc);
+
+//删除指定联系人
+void DelContact(Contact* pc);
+
+//查找指定联系人
+void SearchContact(const Contact* pc);
+
+//文件版本
+//保存通讯录的信息到文件
+void SaveContact(const Contact* pc);

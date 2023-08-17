1aa84813d51b76cdf46ccdfa4756efef042be9c5
iceland_17 <128897003+ice-land17@users.noreply.github.com>
2023-08-17 21:35:15 +0800

    Add files via upload

---
+++ 初阶数据结构/test2.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	//char arr1[] = "bit";
+//	//char arr2[] = { 'b','i','t' };
+//	//char arr3[] = { 'b','i','t','\0' };
+//	//printf("%s\n", arr1);
+//	//printf("%s\n", arr2);
+//	//printf("%s\n", arr3);
+//	printf("\a\a\a\a\a");
+//	return 0;
+//}
+
+//程序输出什么？
+//int main()
+//{
+//	printf("%d\n", strlen("abcdef"));
+//	// \32被解析成一个转义字符
+//	printf("%d\n", strlen("c:\test\328\test.c"));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("你要好好学习吗?(1/0)");
+//	scanf("%d" ,&input);
+//	if (1 == input)
+//	{
+//		printf("好offer\n");
+//	}
+//	else
+//	{
+//		printf("卖红薯\n");
+//	}
+//	return 0;
+//}
+
+//循环语句
+int main()
+{
+    printf("加入比特\n");
+    int line = 0;
+    while (line <= 20000)
+    {
+        line++;
+        printf("我要继续努力敲代码:%d\n",);
+    }
+    if (line > 20000)
+        printf("赢取白富美\n");
+    return 0;
+}

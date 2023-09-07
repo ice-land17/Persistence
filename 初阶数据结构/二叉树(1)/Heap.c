acc6595778dfda77fb2983628796ed4cc0af4dae
iceland_17 <128897003+ice-land17@users.noreply.github.com>
2023-09-07 21:27:19 +0800

    Add files via upload

---
+++ 初阶数据结构/二叉树(1)/Heap.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Heap.h"
+
+void HeapInit(HP* php)
+
+{
+	assert(php);
+	php->a = NULL;
+	php->size = php->capacity = 0;
+}
+
+void HeapDestory(HP* php)
+{
+	assert(php);
+	free(php->a);
+	php->a = NULL;
+	php->size = php->capacity = 0;
+}
+
+void AdjustUp(HPDataType* a, int child)
+{
+	int parent = (child - 1) / 2;
+	while (child > 0)
+	{
+		if (a[child] < a[parent])
+		{
+			Swap(&a[child], &a[parent]);
+			child = parent;
+			parent = (child - 1) / 2;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+void HeapPush(HP* php, HPDataType x)
+{
+	assert(php);
+	if (php->size == php->capacity)
+	{
+		//À©ÈÝ
+	}
+	php->a[php->size] = x;
+	php->size++;
+
+	AdjustUp(php->a, php->size - 1);
+
+}
+
+void HeapPop(HP* php)
+{
+
+}
+
+bool HeapEmpty(HP* php)
+{
+
+}
+
+int HeapSize(HP* php)
+{
+
+}
---
+++ 初阶数据结构/二叉树(1)/Test.c
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Heap.h"
+int main()
+{
+	Heap hp;
+	HeapInit(&hp);
+}

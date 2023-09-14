5a80ac58f5cc76fe730fe3933f85a82545b2e9a7
iceland_17 <128897003+ice-land17@users.noreply.github.com>
2023-09-14 18:05:12 +0800

    Add files via upload

---
+++ 初阶数据结构/二叉树(1)/Heap.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+typedef int HPDataType;
+typedef struct Heap
+{
+	HPDataType* a;
+	int size;
+	int capacity;
+}HP;
+void HeapInit(HP* php);
+void HeapDestory(HP* php);
+void HeapPush(HP* php, HPDataType x);
+void HeapPop(HP* php);
+bool HeapEmpty(HP* php);
+int HeapSize(HP* php);
---
+++ 初阶数据结构/二叉树(1)/Test.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Heap.h"
+int main()
+{
+	Heap hp;
+	HeapInit(&hp);
+}
+
